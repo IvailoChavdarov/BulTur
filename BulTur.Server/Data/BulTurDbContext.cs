3a01284efbacf7fa8b1d77e1a44c497bbbe4658d
Ivaylo <Ivaylo@DESKTOP-3U2PHNR>
2025-02-05 22:48:46 +0200

    Add project files.

---
+++ BulTur.Server/Data/BulTurDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulTur.Server.Data
+{
+    public partial class BulTurDbContext : DbContext
+    {
+        public BulTurDbContext(DbContextOptions<BulTurDbContext> options): base(options)
+        {
+        }
+    }
+}
---
+++ BulTur.Server/Program.cs
@@ -0,0 +1,61 @@
+using BulTur.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulTur.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<BulTurDbContext>(options => options.UseSqlServer(connectionString));
+
+            builder.Services.AddSwaggerGen();
+
+            //To stop problems with Cors while testing
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", policy =>
+                {
+                    policy.WithOrigins("https://localhost:5173") // Add your front-end URL
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+                });
+            });
+            var app = builder.Build();
+
+
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseCors("AllowSpecificOrigin");
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.MapFallbackToFile("/index.html");
+
+            app.Run();
+        }
+    }
+}
